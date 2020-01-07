0d466c4a3eae1fa6e942aa2d2e8a429d4f43d57f
Marnix van den Bent <marnix.van.den.bent@rabobank.nl>
2020-01-07 15:43:37 +0100

    Added GenericStatusCodes enum

---
+++ src/interface/generic-status-codes.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 Coöperatieve Rabobank U.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export enum GenericStatusCodes {
+  /**
+   * In case your plugin ignored
+   * the incoming message.
+   */
+  Ignored = 'ignored',
+
+  /**
+   * Generic, unknown error
+   * Please use a more specific
+   * error code in your plugin!
+   */
+  Error = 'error',
+
+  /**
+   * Use this code once your plugin
+   * successfully finished its work.
+   */
+  Success = 'success'
+}
+
+export default GenericStatusCodes
