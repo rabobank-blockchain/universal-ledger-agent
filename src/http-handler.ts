0a8169eebbc39c662f36f6f6083518ed927338b0
Marnix van den Bent <marnix.van.den.bent@rabobank.nl>
2020-01-07 16:05:42 +0100

    Deprecated HttpHandler

--- src/http-handler.ts
+++ src/http-handler.ts
@@ -16,6 +16,12 @@
 
 import { EventHandler } from '.'
 
+/**
+ * @deprecated Please use the EventHandler
+ * Incase your request is a string, parse it
+ * before calling the EventHandler.
+ * @see https://github.com/rabobank-blockchain/universal-ledger-agent/issues/11
+ */
 export class HttpHandler {
 
   constructor (private eventHandler: EventHandler) {
